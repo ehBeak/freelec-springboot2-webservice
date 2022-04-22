027a334d39d598d75c31a4893c9f64effa7c413c
eunhee <dmsgml9903@gmail.com>
2022-04-22 14:58:12 +0900

    [ADD] 스프링 부트 자동 설정을 위한 메인 클래스

---
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -0,0 +1,14 @@
+package com.jojoldu.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 메인 클래스
+ */
+@SpringBootApplication // 스프링 부트의 자동 설정, 스프링 빈 읽기와 생성을 모두 자동으로 설정, 해당 패키지부터 적용
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);//run: 내장 WAS 실행
+    }
+}
