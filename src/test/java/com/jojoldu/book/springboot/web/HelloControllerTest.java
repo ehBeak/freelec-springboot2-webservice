5cf5c8939d5077629e70706e3d541b338a2d73e5
eunhee <dmsgml9903@gmail.com>
2022-04-23 14:31:50 +0900

    [ADD] Lombok 적용

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -8,8 +8,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
 
 // 테스트 코드 작성
 @ExtendWith(SpringExtension.class) // jUnit에 내장된 실행자 외에 다른 실행자(SpringExtension) 실행시킴 (연결자 역할)
@@ -25,5 +25,21 @@ class HelloControllerTest {
         mvc.perform(get("/hello"))//get요청을 한다는 것, 체이닝이 지원됨 (여러 검증 기능 선언 가능)
                 .andExpect(status().isOk())// mvc.perform의 결과를 검증 - status(200)
                 .andExpect(content().string(hello));// mvc.perform의 결과를 검증 - 응답내용("hello")
+    }
+
+    @Test
+    void return_helloDto() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                        get("/hello/dto")
+                                .param("name", name) // 파라미터 넘기기
+                                .param("amount", String.valueOf(amount)) //String만 허용
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name))) // json응답 값을 필드별로 검증하도록 함
+                .andExpect(jsonPath("$.amount", is(amount)));
+
     }
 }
