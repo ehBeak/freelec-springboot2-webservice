1c44aa94af40e3f7de99cf4e9b8e1f86135d407e
eunhee <dmsgml9903@gmail.com>
2022-04-23 15:37:21 +0900

    [FEAT] Posts 저장 기능

---
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,33 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import com.jojoldu.book.springboot.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Entity 클래스를 Request/Response 클래스로 사용해서는 안된다.
+ */
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
