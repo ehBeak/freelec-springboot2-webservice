a2215338a5a3f77be1afbed048ceddf3dc662620
eunhee <dmsgml9903@gmail.com>
2022-04-23 16:25:57 +0900

    [FEAT] Posts(게시글) 수정기능

---
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,21 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import com.jojoldu.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    // PostsResponseDto는 Entity의 필드 중 일부만 사용하기 때문에 Entity를 받아 필드에 값을 넣는다.
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
