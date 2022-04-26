1febdcf1bddf0984ba713b1413728aa5d218acd8
eunhee <dmsgml9903@gmail.com>
2022-04-26 12:45:47 +0900

    [FEAT] 전체 게시글 조회 기능

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,9 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /**
  * Entity 클래스와 기본 Entity Repository는 함께 위치해야한다.(도메인 패키지에서 함께 관리하자)
@@ -8,4 +11,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
+    //SpringDataJpa에서 제공하지 않는 메소드는 쿼리로 작성해도 된다.
+    @Query("SELECT p FROM Posts  p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
