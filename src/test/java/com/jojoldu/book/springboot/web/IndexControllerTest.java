e89277dd6059066647ed730b83facaa9711a6152
eunhee <dmsgml9903@gmail.com>
2022-04-26 12:00:59 +0900

    [ADD] 기본 페이지, 페이지 테스트 코드

---
+++ src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -0,0 +1,31 @@
+package com.jojoldu.book.springboot.web;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void main_loading() {
+        //when
+        // TestRestTemplate 를 통해서 HTML을 문자열로 받는다.
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+    }
+}
